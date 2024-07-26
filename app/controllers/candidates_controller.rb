2849b1d90c53b79cdf47749bdee22e57456dfa2f
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-26 10:23:28 +0700

    feat: add candidates controller

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,43 @@
+class CandidatesController < ApplicationController
+  # include Auth
+  before_action :set_candidate, only: %i[ show ]
+
+  # def index
+  #   @candidates = Candidate.all.order(:id)
+  # end
+
+  def show
+    fetch_experience
+    fetch_education
+  end
+
+  # def edit
+  # end
+
+  # def update
+  #   if @candidate.update(candidate_params)
+  #     redirect_to candidates_path, notice: 'Candidate was successfully updated.'
+  #   else
+  #     redirect_to edit_candidate_path(@candidate), inertia: { errors: @candidate.errors }
+  #   end
+  # end
+
+  private
+
+  def set_candidate
+    @candidate = Candidate.find(params[:id])
+    @user = @candidate.user
+  end
+
+  def fetch_experience
+    @experiences = Experience.where(candidate: @candidate)
+  end
+
+  def fetch_education
+    @educations = Education.where(candidate: @candidate)
+  end
+
+  # def candidate_params
+  #   params.fetch(:candidate, {}).permit(:first_name, :last_name)
+  # end
+end
