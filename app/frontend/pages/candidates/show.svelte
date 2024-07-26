1fe1f9e4c2eb1b5b539080b850b70fa04d258539
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-26 14:30:53 +0700

    feat: enhance candidate profile page
    - Added sections for photo, contact details, bio, experiences, education, and languages
    - Each section is now encapsulated in a card for better organization and presentation

--- app/frontend/pages/candidates/show.svelte
+++ app/frontend/pages/candidates/show.svelte
@@ -11,20 +11,7 @@
   export let experiences
   export let educations
 
-
-  // Local Data
-  let projects = [
-    {
-      title: 'Project Alpha',
-      description: 'A groundbreaking project in AI.',
-      completion_date: '2024-06-15'
-    },
-    {
-      title: 'Project Beta',
-      description: 'An innovative web development project.',
-      completion_date: '2024-07-20'
-    }
-  ];
+  console.log(candidate.languages)
 
   // Utility functions
   function formatDate(date, format) {
@@ -32,51 +19,59 @@
   }
 </script>
 
-<Card class="p-4 mb-4 rounded-none shadow-none bg-card text-card-foreground">
-  <div>
+<Card class="p-6 mb-4 border-none shadow-none">
+  <div class = "mb-4">
     <Avatar.Root>
       <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" class="h-[100px]"/>
       <Avatar.Fallback>{candidate.first_name}</Avatar.Fallback>
     </Avatar.Root>
   </div>
   <div>
-    <h1 class="mb-4 text-2xl font-bold">{candidate.first_name} {candidate.last_name}</h1>
+    <h1 class="mb-4 text-lg font-bold">{candidate.first_name} {candidate.last_name}</h1>
     <h2 class="font-light">{user.email}</h2>
     <h2 class="font-light">{user.phone}</h2>
     <h2 class="font-light">{user.city}, {user.state}</h2>
   </div>
 </Card>
 
-<Card class="p-4 mb-4 rounded-none shadow-none bg-card text-card-foreground">
+<Card class="p-6 mb-4 bg-white border-none shadow-none">
   <div>
-    <h1 class="mb-4 text-2xl font-bold">Bio</h1>
+    <h1 class="mb-4 text-lg font-bold">Bio</h1>
     <h2 class="font-light">{user.bio}</h2>
   </div>
 </Card>
 
-<Card class="p-4 mb-4 rounded-none shadow-none bg-card text-card-foreground">
-  <h1 class="mb-1 text-2xl font-bold">Experience</h1>
+<Card class="p-6 mb-4 border-none shadow-none">
+  <h1 class="mb-4 text-lg font-bold">Experience</h1>
   {#each experiences as experience}
-    <div class="space-y-1">
-      <h2 class="text-xl font-semibold">{experience.company_name}</h2>
-      <p class="text-lg">{experience.job_title}</p>
+    <div class="mb-12 space-y-1">
+      <p class="text-lg font-medium">{experience.job_title}</p>
+      <h2 class="">{experience.company_name}</h2>
       <p class="text-sm text-gray-500">{formatDate(experience.start_date, 'MMM YYYY')} - {formatDate(experience.end_date, 'MMM YYYY')}</p>
-      <p class="text-base">{experience.description}</p>
+      <p class="text-sm">{experience.description}</p>
     </div>
   {/each}
 </Card>
 
 
-<Card class="p-4 mb-4 rounded-none shadow-none bg-card text-card-foreground">
-  <h1 class="mb-1 text-2xl font-bold">Education</h1>
+<Card class="p-6 mb-4 border-none shadow-none">
+  <h1 class="mb-4 text-lg font-bold">Education</h1>
   {#each educations as education}
     <div class="space-y-1">
-      <p class="text-xl font-semibold">{education.institution_name}</p>
-      <p class="text-lg">{education.degree} in {education.field_of_study}</p>
+      <p class="text-lg font-semibold">{education.institution_name}</p>
+      <p class="text-base">{education.degree} in {education.field_of_study}</p>
       <p class="text-sm text-gray-500">
         {formatDate(education.start_date, 'MMM YYYY')} - {formatDate(education.end_date, 'MMM YYYY')}</p>
-      <p class="text-base">{education.description}</p>
+      <p class="text-sm">{education.description}</p>
     </div>
   {/each}
+</Card>
 
+<Card class="p-6 mb-4 border-none shadow-none">
+  <h1 class="mb-4 text-lg font-bold">Languages</h1>
+  {#each candidate.languages as language}
+    <div class="space-y-1">
+      <p class="text-gray-500"> {language}</p>
+    </div>
+  {/each}
 </Card>
