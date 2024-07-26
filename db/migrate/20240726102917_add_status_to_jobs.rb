c08c612313a4966d9b872a0d11e7f559720967fe
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-26 17:34:25 +0700

    migrate/fix: rollback and rerun migration to add and index and default to job status

--- db/migrate/20240726102917_add_status_to_jobs.rb
+++ db/migrate/20240726102917_add_status_to_jobs.rb
@@ -1,5 +1,6 @@
 class AddStatusToJobs < ActiveRecord::Migration[7.1]
   def change
-    add_column :jobs, :status, :string
+    add_column :jobs, :status, :string, default: 'Draft'
+    add_index :jobs, :status
   end
 end
