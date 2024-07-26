7b015c6b356c2810bb2b889dfcde858827437959
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-26 23:20:36 +0700

    feat: create jobs index page with responsive cards and employer logos
    - Implemented jobs index view with responsive cards displaying job details
    - Added employer logo display on job cards
    - Added placeholder links for job details and 'Apply Now' actions
    - Applied responsive styling to ensure mobile and desktop compatibility
    
    Note: This commit encompasses multiple related features and styling updates. Future commits will break down these changes into smaller, more manageable pieces for better clarity and maintainability.

--- app/frontend/pages/jobs/index.svelte
+++ app/frontend/pages/jobs/index.svelte
@@ -14,3 +14,33 @@
   <p>{job.employer.company_name}</p>
   <p>{job.employer.user.profile_picture}</p>
 {/each} -->
+<h1 class="mx-auto my-24 text-5xl text-center">Discover the <span class="text-red-500">Best Jobs</span></h1>
+
+<div class="grid grid-cols-1 gap-4">
+  {#each jobs as job}
+    <Card.Root class="w-full max-w-full overflow-hidden bg-white rounded-lg shadow-lg">
+        <!-- Avatar -->
+
+        <!-- Card Header -->
+        <div class="w-full">
+          <Card.Header class="flex p-0 pt-6 mt-4">
+            <div class="flex px-4 space-x-6">
+              <Avatar.Root class="w-24 h-24">
+                <Avatar.Image src="{job.employer.user.profile_picture}" alt="Employer Profile Picture" class="object-cover" />
+                <Avatar.Fallback></Avatar.Fallback>
+              </Avatar.Root>
+              <div class="self-center">
+                <Card.Title class="text-xl font-bold text-gray-900">{job.title}</Card.Title>
+                <Card.Description class="text-sm text-gray-600">{job.location} . <span class="text-sm text-gray-500">{job.status}</span></Card.Description>
+              </div>
+            </div>
+          </Card.Header>
+          <!-- Card Footer -->
+          <Card.Footer class="flex justify-end pb-4 border-gray-200">
+            <Button variant="outline" class="mr-2">More Details</Button>
+            <Button>Apply Now</Button>
+          </Card.Footer>
+        </div>
+    </Card.Root>
+  {/each}
+</div>
