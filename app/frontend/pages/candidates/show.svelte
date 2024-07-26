13ef63ceda3be837acf62fd7b4b9d1ffe9cf0850
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-26 16:54:57 +0700

    style: refine aesthetics of candidate profile page
    - Adjusted styling for improved layout and visual appeal
    - Made minor tweaks to card and section designs

--- app/frontend/pages/candidates/show.svelte
+++ app/frontend/pages/candidates/show.svelte
@@ -4,6 +4,7 @@
   import { Link } from '@inertiajs/svelte';
   import { Card } from '$lib/components/ui/card'
   import * as Avatar from "$lib/components/ui/avatar";
+  import { Separator } from "$lib/components/ui/separator";
 
   // Component Props
   export let candidate
@@ -20,9 +21,9 @@
 </script>
 
 <Card class="p-6 mb-4 border-none shadow-none">
-  <div class = "mb-4">
+  <div style="width: 100px height: auto">
     <Avatar.Root>
-      <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" class="h-[100px]"/>
+      <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn"/>
       <Avatar.Fallback>{candidate.first_name}</Avatar.Fallback>
     </Avatar.Root>
   </div>
@@ -37,41 +38,50 @@
 <Card class="p-6 mb-4 bg-white border-none shadow-none">
   <div>
     <h1 class="mb-4 text-lg font-bold">Bio</h1>
-    <h2 class="font-light">{user.bio}</h2>
+    <h2 class="">{user.bio}</h2>
   </div>
 </Card>
 
 <Card class="p-6 mb-4 border-none shadow-none">
-  <h1 class="mb-4 text-lg font-bold">Experience</h1>
-  {#each experiences as experience}
-    <div class="mb-12 space-y-1">
+  <h1 class="text-lg font-bold">Experience</h1>
+  {#each experiences as experience, index}
+    <div class="py-12 space-y-1">
       <p class="text-lg font-medium">{experience.job_title}</p>
       <h2 class="">{experience.company_name}</h2>
       <p class="text-sm text-gray-500">{formatDate(experience.start_date, 'MMM YYYY')} - {formatDate(experience.end_date, 'MMM YYYY')}</p>
       <p class="text-sm">{experience.description}</p>
     </div>
+    {#if index < experiences.length - 1}
+      <Separator />
+    {/if}
   {/each}
 </Card>
 
 
 <Card class="p-6 mb-4 border-none shadow-none">
-  <h1 class="mb-4 text-lg font-bold">Education</h1>
-  {#each educations as education}
-    <div class="space-y-1">
-      <p class="text-lg font-semibold">{education.institution_name}</p>
+  <h1 class="text-lg font-bold">Education</h1>
+  {#each educations as education, index}
+    <div class="py-12 space-y-1">
+      <p class="text-lg font-medium">{education.institution_name}</p>
       <p class="text-base">{education.degree} in {education.field_of_study}</p>
       <p class="text-sm text-gray-500">
         {formatDate(education.start_date, 'MMM YYYY')} - {formatDate(education.end_date, 'MMM YYYY')}</p>
       <p class="text-sm">{education.description}</p>
     </div>
+    {#if index < educations.length - 1}
+      <Separator />
+    {/if}
   {/each}
 </Card>
 
 <Card class="p-6 mb-4 border-none shadow-none">
-  <h1 class="mb-4 text-lg font-bold">Languages</h1>
-  {#each candidate.languages as language}
-    <div class="space-y-1">
+  <h1 class="text-lg font-bold">Languages</h1>
+  {#each candidate.languages as language, index}
+    <div class="py-12 space-y-1">
       <p class="text-gray-500"> {language}</p>
     </div>
+    {#if index < candidate.languages.length - 1}
+      <Separator />
+    {/if}
   {/each}
 </Card>
