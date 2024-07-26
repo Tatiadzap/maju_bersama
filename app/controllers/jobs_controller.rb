7c3d2093beecd90fae61d457a98479f7e9098f4b
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-26 17:27:08 +0700

    feat: implement index and show actions in Jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,50 @@
 class JobsController < ApplicationController
+  # include Auth
+  before_action :set_event, only: %i[ show ]
+
+  def index
+    @events = Job.all
+  end
+
+  def show
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  # def create
+  #   job = Job.new(event_params)
+  #   if job.save
+  #     redirect_to events_path, notice: 'Job created.'
+  #   else
+  #     redirect_to new_event_path, inertia: { errors: job.errors }
+  #   end
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  #   if @job.update(event_params)
+  #     redirect_to events_path, notice: 'Job was successfully updated.'
+  #   else
+  #     redirect_to edit_event_path(@job), inertia: { errors: @job.errors }
+  #   end
+  # end
+
+  # def destroy
+  #   @job.destroy
+  #   redirect_to events_path, notice: 'Job was successfully deleted.'
+  # end
+
+  private
+
+  def set_event
+    @job = Job.find(params[:id])
+  end
+
+  # def event_params
+  #   params.fetch(:job, {}).permit(:title, :description, :start_date)
+  # end
 end
