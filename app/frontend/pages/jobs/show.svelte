2c8d48a0175b109b9844956c8230e072803f7d4e
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-27 01:23:18 +0700

    feat: build initial UI for job show page
    - Developed the job show page with a clean UI layout.
    - Added sections for job description and requirements.
    - Placeholder for additional sections to be implemented later.

--- app/frontend/pages/jobs/show.svelte
+++ app/frontend/pages/jobs/show.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { Separator } from "$lib/components/ui/separator";
+
   export let job
   export let employer
   export let company_details
@@ -7,16 +9,36 @@
   // export let company_details
 </script>
 
-<img src="{company_details.profile_picture}" alt="">
-{job.title}
-{employer.company_name}
-{job.location}
-{job.status}
-{job.updated_at}
-{job.status}
-{job.description}
-{job.requirements}
 
+<div class="flex py-12">
+  <div class="flex space-x-4">
+    <img src="{company_details.profile_picture}" alt="" class="w-24 h-24">
+    <div class="flex flex-col justify-center">
+      <p class="text-xl font-medium">{job.title}</p>
+      <div class="flex space-x-6 row">
+        <p>{employer.company_name}</p>
+        <p>{job.location}</p>
+        <p>Full-time</p>
+        <p>{job.status}</p>
+        <!-- <p>{job.updated_at}</p> -->
 
+      </div>
+    </div>
+  </div>
+</div>
 
+<div class="w-full h-0.5 bg-gray-200"></div>
+<div class="flex my-12">
+  <div class="space-y-12 flex-basis-2/3">
+    <div>
+      <h2 class="mb-2 text-lg font-medium">About the role</h2>
+      <p>{job.description}</p>
+    </div>
+
+    <div>
+      <h2 class="mb-2 text-lg font-medium">Job requirements</h2>
+      <p>{job.requirements}</p>
+    </div>
+  </div>
+</div>
 <!-- {company_details} -->
