38169e52daaaff97f8eb79880540baa6c0e8d41c
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-27 22:57:15 +0700

    feat: create action in JobApplications controller with params

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,2 +1,20 @@
 class JobApplicationsController < ApplicationController
+  include Auth
+
+  def create
+    job_application = JobApplication.new(job_application_params)
+    job_application.user_id = current_user.id
+
+    if job_application.save
+      render json: { message: 'Application submitted successfully' }, status: :created
+    else
+      render json: { errors: job_application.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def job_application_params
+    params.require(:job_application).permit(:job_id, :user_id)
+  end
 end
