356dbef0513f71cd50188f8a4dab38650937b6b5
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-28 00:55:45 +0700

    Create a store to manage the application status of jobs.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_26_102917) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_27_162203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,7 +104,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_26_102917) do
     t.bigint "user_id", null: false
     t.bigint "job_id", null: false
     t.string "status"
-    t.datetime "applied_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["job_id"], name: "index_job_applications_on_job_id"
