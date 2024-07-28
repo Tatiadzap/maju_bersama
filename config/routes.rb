a8379d2eb562cf9a7f87b209617ece371957f62b
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-28 12:43:44 +0700

    feat: go to jobs index page after login

--- config/routes.rb
+++ config/routes.rb
@@ -35,5 +35,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "static#home"
+  # root "static#home"
+  root "jobs#index"
 end
