4aa8d29cf56d3c742c54d19d426491b3b4c6b597
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-08-20 02:47:38 +0700

    feat(Event): add applicants association and status validation
    Added a new association to link applicants through event_registrations and introduced status validation with predefined options. Implemented methods to retrieve candidates and status options.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,18 @@
 class Event < ApplicationRecord
   belongs_to :employer
   has_many :event_registrations, dependent: :destroy
+  has_many :applicants, through: :event_registrations, source: :user
+
 
   validates :name, :start_time, :end_time, presence: true
+  validates :status, inclusion: { in: %w[Open Closed On_Hold Draft] }, presence: true
 
+  def candidates
+    event_registrations.includes(user: :candidate).map { |app| app.user.candidate }
+  end
+
+  def self.status_options
+    %w[Open Closed On_Hold Draft]
+  end
   # Other validations and methods
 end
