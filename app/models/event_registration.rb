af82b6fd0830c5cdb741887e9f2b4bd3db3aac12
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-08-20 13:18:27 +0700

    fix: added validations to event_registration and job application + added first and last name to job application when employer sees it

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -1,5 +1,9 @@
 class EventRegistration < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  validates :status, presence: true
+  validates :event_id, presence: true
+  validates :user_id, presence: true
   # Other validations and methods
 end
