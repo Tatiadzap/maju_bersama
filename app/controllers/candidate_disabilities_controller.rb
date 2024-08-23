923ad3b20f18ce900c76e61f3e09c9f98c573cba
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-08-23 08:20:21 +0700

    feat: add icons to 'disabilities' table
    Added icon support to the 'disabilities' table for improved visual representation.

--- app/controllers/candidate_disabilities_controller.rb
+++ app/controllers/candidate_disabilities_controller.rb
@@ -1,22 +1,27 @@
 class CandidateDisabilitiesController < ApplicationController
-  before_action :set_candidate, only: %i[ create]
-  before_action :set_candidate_disability, only: %i[ edit update destroy]
+  # before_action :set_candidate, only: %i[ create]
+  # before_action :set_candidate_disability, only: %i[ edit update destroy]
 
-  def create
+  # def create
+  #   # @disabilities = @candidate.experiences.new(experience_params)
+  #   # if @experience.save
+  #   #   render json: @experience, status: :created
+  #   # else
+  #   #   render json: @experience.errors, status: :unprocessable_entity
+  #   # end
+  # end
 
-  end
+  # private
 
-  private
+  # def set_candidate
+  #   @candidate = Candidate.find(params[:candidate_id])
+  # end
 
-  def set_candidate
-    @candidate = Candidate.find(params[:candidate_id])
-  end
+  # def set_candidate_disability
+  #   @candidate_disability = CandidateDisability.find(params[:id])
+  # end
 
-  def set_candidate_disability
-    @candidate_disability = CandidateDisability.find(params[:id])
-  end
-
-  def candidate_disabilities_params
-    params.require(:candidate_disability).permit(:candidate_id, :disability_id, :details)
-  end
+  # def candidate_disabilities_params
+  #   params.require(:candidate_disability).permit(:candidate_id, :disability_id, :details)
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_25_040102) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon"
   end
 
   create_table "educations", force: :cascade do |t|
