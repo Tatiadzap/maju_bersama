f5e015dd435edfdaea8b26327ec7ac30cf322fbc
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-08-26 15:21:14 +0700

    fixed incorrect migrations as a result of find and replace all

--- db/migrate/20240823004954_rename_user_disabilities_to_candidate_disabilities.rb
+++ db/migrate/20240823004954_rename_user_disabilities_to_candidate_disabilities.rb
@@ -1,6 +1,6 @@
 class RenameUserDisabilitiesToCandidateDisabilities < ActiveRecord::Migration[7.1]
   def change
-    rename_table :candidate_disabilities, :candidate_disabilities
+    rename_table :user_disabilities, :candidate_disabilities
     rename_column :candidate_disabilities, :user_id, :candidate_id
   end
 end
