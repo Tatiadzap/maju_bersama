a9e223738abc2fcfee5eea8d7da6a07818e76f7b
Tatiadzap <dzaptash@gmail.com>
2024-08-27 10:12:14 -0400

    Update 20240725040526_create_candidate_disabilities.rb

--- db/migrate/20240725040526_create_candidate_disabilities.rb
+++ db/migrate/20240725040526_create_candidate_disabilities.rb
@@ -1,12 +1,1 @@
-class CreateCandidateDisabilities < ActiveRecord::Migration[7.1]
-  def change
-    create_table :candidate_disabilities do |t|
-      t.references :candidate, null: false, foreign_key: true, index: true
-      t.references :disability, null: false, foreign_key: true, index: true
-      t.text :details
-
-      t.timestamps
-    end
-    add_index :candidate_disabilities, [:candidate_id, :disability_id], unique: true
-  end
-end
+If this is Alden, please respond. I've been trying to reach you everywhere
